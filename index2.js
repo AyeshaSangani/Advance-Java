0b944cb3adc6d02a9ddad8c7d8d2333a94ca8d08
AyeshaSangani <142738168+AyeshaSangani@users.noreply.github.com>
2024-12-03 23:45:55 +0500

    parseFloat & Eval

---
+++ index2.js
@@ -0,0 +1,19 @@
+//   ==============!!!!!!!!!!!     PARSEFLOAT AND EVAL  !!!!!!!============
+
+
+// without spacing they work same
+
+let a = parseFloat("15"+"15")
+
+// let a = eval ("22"+"22")
+
+console.log(a);
+
+// with spacing spacing parse float get only 1 value while eval sum the Number
+
+
+// let a = parseFloat("15" + "15")
+
+// let a = eval ("22" + "22")
+
+console.log(a);
