dc8244a788f8536dc40a14837b59c988eb9ad40e
AyeshaSangani <142738168+AyeshaSangani@users.noreply.github.com>
2024-12-03 23:47:56 +0500

    window , window.history , window.location

---
+++ script.js
@@ -0,0 +1,18 @@
+   
+// ============ARROW FUNCTION MOSTLY USED IN JAVA=========
+
+let run = ()=>{
+    alert("Are You Sure")
+}
+       
+
+ 
+let a = window 
+console.log(a);
+
+let h = window.history
+console.log(h);
+
+let l = window.location
+console.log(l);
+
