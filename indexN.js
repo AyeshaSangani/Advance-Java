84629b5d82ea621f90c099ea8ef72cf42b57df05
AyeshaSangani <142738168+AyeshaSangani@users.noreply.github.com>
2024-12-03 23:51:20 +0500

    nested loop

---
+++ indexN.js
@@ -0,0 +1,38 @@
+for(let i = 1; i<10; i++){
+   
+        console.log(i)    
+}
+console.log(i);
+
+
+
+for(let i = 1; i<10; i++){
+    for(let k = i; k>10; k++){
+        console.log(k)
+    }
+}
+console.log(i);
+
+
+
+for( i = 1; i<10; i++){
+    for( k = i; k>10; k++){
+        console.log(i)
+    }
+}
+console.log(k);
+
+
+
+// enter a number from there loop started
+  
+let a = prompt ("enter number")
+for(i=a; i<10; i++){
+    console.log(i);
+    
+}
+
+
+
+
+
